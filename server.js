7742134305bd7efffa4b63a86e4f2d7ddc3aeeda
justalexok <76539791+justalexok@users.noreply.github.com>
2023-07-10 18:24:14 +0100

    edit server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the index.html file for all routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server

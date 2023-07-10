c9bfa9ad61a03749462a27d7e672639cc44024d6
justalexok <76539791+justalexok@users.noreply.github.com>
2023-07-10 18:24:40 +0100

    edit server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve the static files
+app.use(express.static(__dirname));
 
 // Serve the index.html file for all routes
 app.get('*', (req, res) => {
